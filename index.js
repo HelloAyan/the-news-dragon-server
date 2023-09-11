7a3e74d9f52479c0a44819e1c460ddf1e7568bf8
helloayan <18103373@iubat.edu>
2023-09-11 18:48:42 +0600

    created news & specific news API, category & specific category API

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 var cors = require('cors');
 const port = process.env.PORT || 5000;
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
@@ -13,6 +14,31 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
     res.send(categories);
 })
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedNews = news.find(f => f._id === id);
+    res.send(selectedNews);
+})
+
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id);
+    if (id === 0) {
+        res.send(news);
+    } else {
+        const selectedCategory = news.filter(n => parseInt(n.category_id) === id);
+        res.send(selectedCategory);
+    }
+
+})
+
 app.listen(port, () => {
     console.log(`App is running on port: ${port}`);
 })
